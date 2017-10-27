766dc9a8e342a1433bdc06b651dff4431ad585c8
gwemz <gwem.win@gmail.com>
2017-10-27 13:46:18 +0800

    'lazyload'

--- js/index.js
+++ js/index.js
@@ -4,11 +4,16 @@ $(function () {
     $.getJSON(flickerAPI, {}).done(function (data) {
         $.each(data.STORIES.stories, function (i, item) {
             // $('<div class="title" id=' + item.id + '><span>' + item.title + '</span><img class="img" src="' + item.images[0] + '"></img></div>').appendTo($('#content'));
-            $('<li><div class="row"><div class="col-sm-6 col-md-4"><div class="thumbnail gwt_home" id="'+item.id+'"><img src="'+item.images[0]+'" alt="加载失败"><div class="caption"><p>'+item.title+'</p></div></div></div></div></li>').appendTo($('#thelist'));
+            $('<li><div class="row"><div class="col-sm-6 col-md-4"><div class="thumbnail gwt_home" id="'+item.id+'"><img class="lazy" data-original="'+item.images[0]+'" src="" alt="加载失败"><div class="caption"><p>'+item.title+'</p></div></div></div></div></li>').appendTo($('#thelist'));
         })
-        setTimeout(function(){
+        // setTimeout(function(){
             loaded()
-        },1000)
+            $("img.lazy").lazyload({
+                effect:"fadeIn",
+                threshold : 400,
+                container: $("#wrapper")
+            });
+        // },1000)
     });
 
     $('#page_content').on('click', '.back', function () {
@@ -28,9 +33,9 @@ $(function () {
             $('.page_content')[0].innerHTML = data.CONTENTS.body;
             $('.headline').remove();
             myScroll2 = {};
-            setTimeout(function(){
+            // setTimeout(function(){
                 loaded1();
-            },1000)
+            // },1000)
         })
     })
 
